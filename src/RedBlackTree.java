b6ea2082670de27624e6031c686d68d60720b201
Bautista Baiocchi-Lora <Bautista-Baiocchi-lora@users.noreply.github.com>
2019-05-27 17:12:35 -0300

    refrator code and git ignore

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -1,413 +1,374 @@
-
 import java.io.Serializable;
 
 public class RedBlackTree<T extends Comparable<T>> implements Serializable {
 
-    private class Node<T extends Comparable<T>> implements Serializable {
-        Comparable<T> data; // holds the key
-        Node parent; // pointer to the parent
-        Node left; // pointer to left child
-        Node right; // pointer to right child
-        int color; // 1 . Red, 0 . Black
-        boolean isDead;
-    }
+	private Node root;
+	private Node TNULL;
 
-    public boolean nodeIsDead(){
-        return root.isDead;
-    }
+	public RedBlackTree() {
+		TNULL = new Node();
+		TNULL.color = 0;
+		TNULL.left = null;
+		TNULL.right = null;
+		root = TNULL;
+	}
 
-    private Node root;
-    private Node TNULL;
+	public boolean nodeIsDead() {
+		return root.isDead;
+	}
 
-    public RedBlackTree() {
-        TNULL = new Node();
-        TNULL.color = 0;
-        TNULL.left = null;
-        TNULL.right = null;
-        root = TNULL;
-    }
+	public boolean isEmpty() {
+		return root.data == null;
+	}
 
-    public boolean isEmpty() {
-        return root.data == null;
-    }
+	public void compact() {
+		searchDeadNode(this.root);
+	}
 
-    public void compact() {
-        searchDeadNode(this.root);
-    }
+	public void killNode(Comparable<T> k) {
+		search(this.root, k).isDead = true;
+	}
 
-    public void killNode(Comparable<T> k) {
-        search(this.root, k).isDead = true;
-    }
+	private void searchDeadNode(Node node) {
+		if (node != null) {
+			if (node.isDead) {
+				deleteNode(node.data);
+			}
+			searchDeadNode(node.left);
+			searchDeadNode(node.right);
+		}
+	}
 
-    private void searchDeadNode(Node node) {
-        if (node != null){
-            if (node.isDead)
-                deleteNode(node.data);
-            searchDeadNode(node.left);
-            searchDeadNode(node.right);
-        }
-    }
+	private void fixDelete(Node x) {
+		Node s;
+		while (x != root && x.color == 0) {
+			if (x == x.parent.left) {
+				s = x.parent.right;
+				if (s.color == 1) {
+					s.color = 0;
+					x.parent.color = 1;
+					leftRotate(x.parent);
+					s = x.parent.right;
+				}
 
-    // logicDelete the node from the tree
-    public void deleteNode(Comparable<T> data) {
-        deleteNodeHelper(this.root, data);
-    }
+				if (s.left.color == 0 && s.right.color == 0) {
+					s.color = 1;
+					x = x.parent;
+				} else {
+					if (s.right.color == 0) {
+						s.left.color = 0;
+						s.color = 1;
+						rightRotate(s);
+						s = x.parent.right;
+					}
 
-    public RedBlackTree<T> getLeft() {
-        RedBlackTree<T> r =  new RedBlackTree<>();
-        r.root = root.left;
-        return r;
-    }
+					s.color = x.parent.color;
+					x.parent.color = 0;
+					s.right.color = 0;
+					leftRotate(x.parent);
+					x = root;
+				}
+			} else {
+				s = x.parent.left;
+				if (s.color == 1) {
+					// case 3.1
+					s.color = 0;
+					x.parent.color = 1;
+					rightRotate(x.parent);
+					s = x.parent.left;
+				}
 
-    public RedBlackTree<T> getRight() {
-        RedBlackTree<T> r =  new RedBlackTree<>();
-        r.root = root.right;
-        return r;
-    }
+				if (s.right.color == 0 && s.right.color == 0) {
+					s.color = 1;
+					x = x.parent;
+				} else {
+					if (s.left.color == 0) {
+						s.right.color = 0;
+						s.color = 1;
+						leftRotate(s);
+						s = x.parent.left;
+					}
+					s.color = x.parent.color;
+					x.parent.color = 0;
+					s.left.color = 0;
+					rightRotate(x.parent);
+					x = root;
+				}
+			}
+		}
+		x.color = 0;
+	}
 
-    private Node search(Node node, Comparable<T> key) {
-        if (node == TNULL) {
-            throw new RuntimeException("Could not find the element");
-        }
-        if (key.compareTo((T) node.data) == 0)
-            return node;
+	// logicDelete the node from the tree
+	public void deleteNode(Comparable<T> data) {
+		deleteNodeHelper(this.root, data);
+	}
 
-        if (key.compareTo((T) node.data) < 0) {
-            return search(node.left, key);
-        }
-        return search(node.right, key);
-    }
+	public RedBlackTree<T> getLeft() {
+		RedBlackTree<T> r = new RedBlackTree<>();
+		r.root = root.left;
+		return r;
+	}
 
-    // fix the rb tree modified by the logicDelete operation
-    private void fixDelete(Node x) {
-        Node s;
-        while (x != root && x.color == 0) {
-            if (x == x.parent.left) {
-                s = x.parent.right;
-                if (s.color == 1) {
-                    // case 3.1
-                    s.color = 0;
-                    x.parent.color = 1;
-                    leftRotate(x.parent);
-                    s = x.parent.right;
-                }
+	public RedBlackTree<T> getRight() {
+		RedBlackTree<T> r = new RedBlackTree<>();
+		r.root = root.right;
+		return r;
+	}
 
-                if (s.left.color == 0 && s.right.color == 0) {
-                    // case 3.2
-                    s.color = 1;
-                    x = x.parent;
-                } else {
-                    if (s.right.color == 0) {
-                        // case 3.3
-                        s.left.color = 0;
-                        s.color = 1;
-                        rightRotate(s);
-                        s = x.parent.right;
-                    }
+	private Node search(Node node, Comparable<T> key) {
+		if (node == TNULL) {
+			throw new RuntimeException("No element found...");
+		}
+		if (key.compareTo((T) node.data) == 0)
+			return node;
 
-                    // case 3.4
-                    s.color = x.parent.color;
-                    x.parent.color = 0;
-                    s.right.color = 0;
-                    leftRotate(x.parent);
-                    x = root;
-                }
-            } else {
-                s = x.parent.left;
-                if (s.color == 1) {
-                    // case 3.1
-                    s.color = 0;
-                    x.parent.color = 1;
-                    rightRotate(x.parent);
-                    s = x.parent.left;
-                }
+		if (key.compareTo((T) node.data) < 0) {
+			return search(node.left, key);
+		}
+		return search(node.right, key);
+	}
 
-                if (s.right.color == 0 && s.right.color == 0) {
-                    // case 3.2
-                    s.color = 1;
-                    x = x.parent;
-                } else {
-                    if (s.left.color == 0) {
-                        // case 3.3
-                        s.right.color = 0;
-                        s.color = 1;
-                        leftRotate(s);
-                        s = x.parent.left;
-                    }
+	private void rbTransplant(Node u, Node v) {
+		if (u.parent == null) {
+			root = v;
+		} else if (u == u.parent.left) {
+			u.parent.left = v;
+		} else {
+			u.parent.right = v;
+		}
+		v.parent = u.parent;
+	}
 
-                    // case 3.4
-                    s.color = x.parent.color;
-                    x.parent.color = 0;
-                    s.left.color = 0;
-                    rightRotate(x.parent);
-                    x = root;
-                }
-            }
-        }
-        x.color = 0;
-    }
+	private void deleteNodeHelper(Node node, Comparable<T> key) {
+		Node z = TNULL;
+		Node x, y;
+		while (node != TNULL) {
+			if (node.data == key) {
+				z = node;
+			}
 
+			if (node.data.compareTo(key) <= 1) {
+				node = node.right;
+			} else {
+				node = node.left;
+			}
+		}
 
-    private void rbTransplant(Node u, Node v){
-        if (u.parent == null) {
-            root = v;
-        } else if (u == u.parent.left){
-            u.parent.left = v;
-        } else {
-            u.parent.right = v;
-        }
-        v.parent = u.parent;
-    }
+		if (z == TNULL) {
+			System.out.println("Couldn't find key in the tree");
+			return;
+		}
 
-    private void deleteNodeHelper(Node node, Comparable<T> key) {
-        // find the node containing key
-        Node z = TNULL;
-        Node x, y;
-        while (node != TNULL){
-            if (node.data == key) {
-                z = node;
-            }
+		y = z;
+		int yOriginalColor = y.color;
+		if (z.left == TNULL) {
+			x = z.right;
+			rbTransplant(z, z.right);
+		} else if (z.right == TNULL) {
+			x = z.left;
+			rbTransplant(z, z.left);
+		} else {
+			y = minimum(z.right);
+			yOriginalColor = y.color;
+			x = y.right;
+			if (y.parent == z) {
+				x.parent = y;
+			} else {
+				rbTransplant(y, y.right);
+				y.right = z.right;
+				y.right.parent = y;
+			}
 
-            if (node.data.compareTo(key) <= 1) {
-                node = node.right;
-            } else {
-                node = node.left;
-            }
-        }
+			rbTransplant(z, y);
+			y.left = z.left;
+			y.left.parent = y;
+			y.color = z.color;
+		}
+		if (yOriginalColor == 0) {
+			fixDelete(x);
+		}
+	}
 
-        if (z == TNULL) {
-            System.out.println("Couldn't find key in the tree");
-            return;
-        }
+	private void fixInsert(Node k) {
+		Node u;
+		while (k.parent.color == 1) {
+			if (k.parent == k.parent.parent.right) {
+				u = k.parent.parent.left;
+				if (u.color == 1) {
+					u.color = 0;
+					k.parent.color = 0;
+					k.parent.parent.color = 1;
+					k = k.parent.parent;
+				} else {
+					if (k == k.parent.left) {
+						k = k.parent;
+						rightRotate(k);
+					}
+					// case 3.2.1
+					k.parent.color = 0;
+					k.parent.parent.color = 1;
+					leftRotate(k.parent.parent);
+				}
+			} else {
+				u = k.parent.parent.right;
 
-        y = z;
-        int yOriginalColor = y.color;
-        if (z.left == TNULL) {
-            x = z.right;
-            rbTransplant(z, z.right);
-        } else if (z.right == TNULL) {
-            x = z.left;
-            rbTransplant(z, z.left);
-        } else {
-            y = minimum(z.right);
-            yOriginalColor = y.color;
-            x = y.right;
-            if (y.parent == z) {
-                x.parent = y;
-            } else {
-                rbTransplant(y, y.right);
-                y.right = z.right;
-                y.right.parent = y;
-            }
+				if (u.color == 1) {
+					// mirror case 3.1
+					u.color = 0;
+					k.parent.color = 0;
+					k.parent.parent.color = 1;
+					k = k.parent.parent;
+				} else {
+					if (k == k.parent.right) {
+						k = k.parent;
+						leftRotate(k);
+					}
+					k.parent.color = 0;
+					k.parent.parent.color = 1;
+					rightRotate(k.parent.parent);
+				}
+			}
+			if (k == root) {
+				break;
+			}
+		}
+		root.color = 0;
+	}
 
-            rbTransplant(z, y);
-            y.left = z.left;
-            y.left.parent = y;
-            y.color = z.color;
-        }
-        if (yOriginalColor == 0){
-            fixDelete(x);
-        }
-    }
+	public T search(Comparable<T> k) {
+		return (T) search(this.root, k).data;
+	}
 
-    // fix the red-black tree
-    private void fixInsert(Node k){
-        Node u;
-        while (k.parent.color == 1) {
-            if (k.parent == k.parent.parent.right) {
-                u = k.parent.parent.left; // uncle
-                if (u.color == 1) {
-                    // case 3.1
-                    u.color = 0;
-                    k.parent.color = 0;
-                    k.parent.parent.color = 1;
-                    k = k.parent.parent;
-                } else {
-                    if (k == k.parent.left) {
-                        // case 3.2.2
-                        k = k.parent;
-                        rightRotate(k);
-                    }
-                    // case 3.2.1
-                    k.parent.color = 0;
-                    k.parent.parent.color = 1;
-                    leftRotate(k.parent.parent);
-                }
-            } else {
-                u = k.parent.parent.right; // uncle
+	public Node minimum(Node node) {
+		while (node.left != TNULL) {
+			node = node.left;
+		}
+		return node;
+	}
 
-                if (u.color == 1) {
-                    // mirror case 3.1
-                    u.color = 0;
-                    k.parent.color = 0;
-                    k.parent.parent.color = 1;
-                    k = k.parent.parent;
-                } else {
-                    if (k == k.parent.right) {
-                        // mirror case 3.2.2
-                        k = k.parent;
-                        leftRotate(k);
-                    }
-                    // mirror case 3.2.1
-                    k.parent.color = 0;
-                    k.parent.parent.color = 1;
-                    rightRotate(k.parent.parent);
-                }
-            }
-            if (k == root) {
-                break;
-            }
-        }
-        root.color = 0;
-    }
+	public Node maximum(Node node) {
+		while (node.right != TNULL) {
+			node = node.right;
+		}
+		return node;
+	}
 
-    // search the tree for the key k
-    // and return the corresponding node
-    public T search(Comparable<T> k) {
-        return (T) search(this.root, k).data;
-    }
+	public Node successor(Node x) {
+		if (x.right != TNULL) {
+			return minimum(x.right);
+		}
 
-    // find the node with the minimum key
-    public Node minimum(Node node) {
-        while (node.left != TNULL) {
-            node = node.left;
-        }
-        return node;
-    }
+		Node y = x.parent;
+		while (y != TNULL && x == y.right) {
+			x = y;
+			y = y.parent;
+		}
+		return y;
+	}
 
-    // find the node with the maximum key
-    public Node maximum(Node node) {
-        while (node.right != TNULL) {
-            node = node.right;
-        }
-        return node;
-    }
+	public Node predecessor(Node x) {
+		if (x.left != TNULL) {
+			return maximum(x.left);
+		}
 
-    // find the successor of a given node
-    public Node successor(Node x) {
-        // if the right subtree is not null,
-        // the successor is the leftmost node in the
-        // right subtree
-        if (x.right != TNULL) {
-            return minimum(x.right);
-        }
+		Node y = x.parent;
+		while (y != TNULL && x == y.left) {
+			x = y;
+			y = y.parent;
+		}
 
-        // else it is the lowest ancestor of x whose
-        // left child is also an ancestor of x.
-        Node y = x.parent;
-        while (y != TNULL && x == y.right) {
-            x = y;
-            y = y.parent;
-        }
-        return y;
-    }
+		return y;
+	}
 
-    // find the predecessor of a given node
-    public Node predecessor(Node x) {
-        // if the left subtree is not null,
-        // the predecessor is the rightmost node in the
-        // left subtree
-        if (x.left != TNULL) {
-            return maximum(x.left);
-        }
+	public void leftRotate(Node x) {
+		Node y = x.right;
+		x.right = y.left;
+		if (y.left != TNULL) {
+			y.left.parent = x;
+		}
+		y.parent = x.parent;
+		if (x.parent == null) {
+			this.root = y;
+		} else if (x == x.parent.left) {
+			x.parent.left = y;
+		} else {
+			x.parent.right = y;
+		}
+		y.left = x;
+		x.parent = y;
+	}
 
-        Node y = x.parent;
-        while (y != TNULL && x == y.left) {
-            x = y;
-            y = y.parent;
-        }
+	public void rightRotate(Node x) {
+		Node y = x.left;
+		x.left = y.right;
+		if (y.right != TNULL) {
+			y.right.parent = x;
+		}
+		y.parent = x.parent;
+		if (x.parent == null) {
+			this.root = y;
+		} else if (x == x.parent.right) {
+			x.parent.right = y;
+		} else {
+			x.parent.left = y;
+		}
+		y.right = x;
+		x.parent = y;
+	}
 
-        return y;
-    }
+	public void insert(Comparable<T> key) {
+		Node node = new Node();
+		node.parent = null;
+		node.data = key;
+		node.left = TNULL;
+		node.right = TNULL;
+		node.color = 1;
 
-    // rotate left at node x
-    public void leftRotate(Node x) {
-        Node y = x.right;
-        x.right = y.left;
-        if (y.left != TNULL) {
-            y.left.parent = x;
-        }
-        y.parent = x.parent;
-        if (x.parent == null) {
-            this.root = y;
-        } else if (x == x.parent.left) {
-            x.parent.left = y;
-        } else {
-            x.parent.right = y;
-        }
-        y.left = x;
-        x.parent = y;
-    }
+		Node y = null;
+		Node x = this.root;
 
-    // rotate right at node x
-    public void rightRotate(Node x) {
-        Node y = x.left;
-        x.left = y.right;
-        if (y.right != TNULL) {
-            y.right.parent = x;
-        }
-        y.parent = x.parent;
-        if (x.parent == null) {
-            this.root = y;
-        } else if (x == x.parent.right) {
-            x.parent.right = y;
-        } else {
-            x.parent.left = y;
-        }
-        y.right = x;
-        x.parent = y;
-    }
+		while (x != TNULL) {
+			y = x;
+			if (node.data.compareTo(x.data) < 0) {
+				x = x.left;
+			} else {
+				x = x.right;
+			}
+		}
 
-    // insert the key to the tree in its appropriate position
-    // and fix the tree
-    public void insert(Comparable<T> key) {
-        // Ordinary Binary Search Insertion
-        Node node = new Node();
-        node.parent = null;
-        node.data = key;
-        node.left = TNULL;
-        node.right = TNULL;
-        node.color = 1; // new node must be red
+		node.parent = y;
+		if (y == null) {
+			root = node;
+		} else if (node.data.compareTo(y.data) < 0) {
+			y.left = node;
+		} else {
+			y.right = node;
+		}
 
-        Node y = null;
-        Node x = this.root;
+		if (node.parent == null) {
+			node.color = 0;
+			return;
+		}
 
-        while (x != TNULL) {
-            y = x;
-            if (node.data.compareTo(x.data) < 0) {
-                x = x.left;
-            } else {
-                x = x.right;
-            }
-        }
+		if (node.parent.parent == null) {
+			return;
+		}
 
-        // y is parent of x
-        node.parent = y;
-        if (y == null) {
-            root = node;
-        } else if (node.data.compareTo(y.data) < 0) {
-            y.left = node;
-        } else {
-            y.right = node;
-        }
+		fixInsert(node);
+	}
 
-        // if new node is a root node, simply return
-        if (node.parent == null){
-            node.color = 0;
-            return;
-        }
+	public T getRoot() {
+		return (T) this.root.data;
+	}
 
-        // if the grandparent is null, simply return
-        if (node.parent.parent == null) {
-            return;
-        }
-
-        // Fix the tree
-        fixInsert(node);
-    }
-
-    public T getRoot(){
-        return (T) this.root.data;
-    }
+	private class Node<T extends Comparable<T>> implements Serializable {
+		Comparable<T> data;
+		Node parent;
+		Node left;
+		Node right;
+		int color;
+		boolean isDead;
+	}
 
 }
